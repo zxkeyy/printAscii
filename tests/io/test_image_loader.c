818ffac5f84b5111968b623a939926b154225108
zxkeyy <zakaria.a.selmani@gmail.com>
2026-04-20 22:04:50 +0100

    fix file name, update image test path resolution

--- tests/io/test_image_loader.c
+++ tests/io/test_image_loader.c
@@ -2,9 +2,15 @@
 #include "io/image_loader.h"
 #include "core/image.h"
 
+#ifndef TEST_IMAGES_DIR
+#define TEST_IMAGES_DIR "tests/images"
+#endif
+
+#define TEST_IMAGE_PATH(name) TEST_IMAGES_DIR "/" name
+
 TEST_CASE(test_image_load_valid_file) {
     // Load a valid image file
-    Image* img = image_load_from_file("tests/images/rgb_100x100.png");
+    Image* img = image_load_from_file(TEST_IMAGE_PATH("rgb_100x100.png"));
     ASSERT_NOT_NULL(img);
     ASSERT_EQUAL(img->width, 100);
     ASSERT_EQUAL(img->height, 100);
@@ -15,33 +21,33 @@ TEST_CASE(test_image_load_valid_file) {
 
 TEST_CASE(test_image_load_invalid_file) {
     // Attempt to load an invalid image file
-    Image* img = image_load_from_file("tests/images/invalid_image.png");
+    Image* img = image_load_from_file(TEST_IMAGE_PATH("invalid_image.png"));
     ASSERT_NULL(img);
     return true;
 }
 
 TEST_CASE(test_image_load_unsupported_format) {
     // Attempt to load an unsupported image format
-    Image* img = image_load_from_file("tests/images/rgb_100x100.webp");
+    Image* img = image_load_from_file(TEST_IMAGE_PATH("rgb_100x100.webp"));
     ASSERT_NULL(img);
     return true;
 }
 
 TEST_CASE(test_image_load_correct_type) {
     // Grayscale image should load as grayscale
-    Image* img_gray = image_load_from_file("tests/images/grayscale_100x100.png");
+    Image* img_gray = image_load_from_file(TEST_IMAGE_PATH("grayscale_100x100.png"));
     ASSERT_NOT_NULL(img_gray);
     ASSERT_EQUAL(img_gray->type, IMAGE_TYPE_GRAY);
     ASSERT_EQUAL(img_gray->channels, 1);
     image_free(img_gray);
     // RGB image should load as RGB
-    Image* img_rgb = image_load_from_file("tests/images/rgb_100x100.png");
+    Image* img_rgb = image_load_from_file(TEST_IMAGE_PATH("rgb_100x100.png"));
     ASSERT_NOT_NULL(img_rgb);
     ASSERT_EQUAL(img_rgb->type, IMAGE_TYPE_RGB);
     ASSERT_EQUAL(img_rgb->channels, 3);
     image_free(img_rgb);
     // RGBA image should load as RGBA
-    Image* img_rgba = image_load_from_file("tests/images/rgba_100x100.png");
+    Image* img_rgba = image_load_from_file(TEST_IMAGE_PATH("rgba_100x100.png"));
     ASSERT_NOT_NULL(img_rgba);
     ASSERT_EQUAL(img_rgba->type, IMAGE_TYPE_RGBA);
     ASSERT_EQUAL(img_rgba->channels, 4);
